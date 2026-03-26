0d4a183d22c483e3e3ef99ddc9bd0f25de985062
KazuKira06 <eplayzgamer@gmail.com>
2026-03-26 23:05:25 +0800

    idk

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
 </head>
 <body>
         <!-- LOGIN SCREEN -->
-        <form action ="login-process.php" method="POST">
+        <form class="login-screen" action ="login-process.php" method="POST">
         <div class="login-card">
             <div class="login-logo">JC</div>
             <h2>Login</h2>
@@ -16,11 +16,11 @@
             <div class="login-form">
             <div class="field-group">
                 <label>Username</label>
-                <input type="text" id="loginUser" placeholder="username" autocomplete="username">
+                <input type="text" id="loginUser" name="username" placeholder="username" autocomplete="username">
             </div>
             <div class="field-group">
                 <label>Password</label>
-                <input type="password" id="loginPass" placeholder="••••••••" autocomplete="current-password">
+                <input type="password" id="loginPass" name="password" placeholder="••••••••" autocomplete="current-password">
             </div>
             <div class="category">
                 <input type="radio" id="admin" name="category" value="admin" required>
